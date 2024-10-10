f9514aae5ef59a98252012ba44a42328edd79591
jusillrose <jhermanoche1920@gmail.com>
2024-10-10 13:15:23 +0800

    Added hands on activities

---
+++ PrintArrayElements.java
@@ -0,0 +1,9 @@
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        String[] names = {"Ali", "Dre", "Chan", "Dee"};
+        
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
